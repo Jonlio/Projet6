5bd2e0be12306f1d9cd0784de6354edc3fcf9a84
Jo <jo-lion@hotmail.fr>
2020-07-19 11:46:12 +0200

    app -v soutenance

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -35,7 +35,7 @@ exports.login = (req, res, next) => {
             if (!valid) {
                 return res.status(401).json({ error: 'Mot de passe incorrect!' });
             }
-            res.status(201).json({ userId: user._id, token: jwt.sign ({ userId: user._id }, config.secret, { expiresIn: '24h' }) });
+            res.status(200).json({ userId: user._id, token: jwt.sign ({ userId: user._id }, config.secret, { expiresIn: '24h' }) });
         })
         .catch(error => res.status(500).json({ error }));
         })
